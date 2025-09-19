d84290e81f433c96269d572615f29b561796509b
Lívia <loc5@aluno.ifnmg.edu.br>
2025-09-19 02:30:05 +0000

    Adiciona server.js com rota raiz e teste do banco

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
-import express from "express";      // Requisição do pacote do express
-const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
+// ######
+// Importando dependências
+// ######
+import express from "express";      // Pacote Express para criar o servidor
+import pkg from "pg";               // Pacote do PostgreSQL
+import dotenv from "dotenv";        // Pacote para lidar com variáveis de ambiente
 
-app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
+// ######
+// Configurações iniciais
+// ######
+const app = express();              // Instancia o Express
+const port = 3000;                  // Define a porta do servidor
+dotenv.config();                    // Carrega as variáveis do arquivo .env
+const { Pool } = pkg;               // Classe Pool para conectar ao Postgres
+
+// ######
+// Definição das rotas
+// ######
+app.get("/", async (req, res) => {  // Torna a função assíncrona para usar await
   console.log("Rota GET / solicitada");
+
+  // Criar nova conexão com o banco
+  const db = new Pool({
+    connectionString: process.env.URL_BD, // Usa a Connection String do .env
+  });
+
+  let dbStatus = "ok";
+
+  // Testar a conexão
+  try {
+    await db.query("SELECT 1");     // Testa se o banco responde
+  } catch (e) {
+    dbStatus = e.message;           // Se der erro, guarda a mensagem
+  }
+
+  // Resposta JSON
   res.json({
-		message: "API para entregar a atividade",      // Substitua pelo conteúdo da sua API
-    author: "Lívia_Oliveira_Cunha",    // Substitua pelo seu nome
+    descricao: "API para entregar a atividade",   // Pode personalizar
+    autor: "Lívia Oliveira Cunha",                // Seu nome completo
+    statusBD: dbStatus                            // Status do banco
   });
 });
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
-  console.log(`Serviço rodando na porta:  ${port}`);
+// ######
+// Servidor escutando
+// ######
+app.listen(port, () => {
+  console.log(`Serviço rodando na porta: ${port}`);
 });
